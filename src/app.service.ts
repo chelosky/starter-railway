4dd0d2767be31da820402959208058f179761961
Chelosky <marcelolazo19972013@hotmail.com>
2022-06-25 17:51:45 -0400

    :sparkles: added service

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,10 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! railway';
+    return 'Hello World! railway 2.0';
+  }
+
+  async test(): Promise<string> {
+    return 'a';
   }
 }
