c8073909a8424c570d8c9716a6896b7c216496d9
Chelosky <marcelolazo19972013@hotmail.com>
2022-06-25 18:10:22 -0400

    :bug: alter throw

--- src/app.service.ts
+++ src/app.service.ts
@@ -9,32 +9,28 @@ export class AppService {
   }
 
   async test(): Promise<string> {
-    try {
-      puppeteer.use(Adblocker({ blockTrackers: true }));
-      const browser = await puppeteer.launch({
-        headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      });
-      const page = await browser.newPage();
-      await page.setUserAgent(
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
-      );
-      const url = 'https://lectortmo.com/library/manga/9276/one-punch-man';
-      await page.goto(url);
+    puppeteer.use(Adblocker({ blockTrackers: true }));
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
+    const page = await browser.newPage();
+    await page.setUserAgent(
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
+    );
+    const url = 'https://lectortmo.com/library/manga/9276/one-punch-man';
+    await page.goto(url);
 
-      const mangaUrl = await page.$eval('ul.chapter-list', (container) => {
-        const aTag = container
-          ?.querySelector('li')
-          ?.querySelector('div')
-          ?.querySelector('div:last-child')
-          ?.querySelector('a');
-        return aTag?.getAttribute('href');
-      });
+    const mangaUrl = await page.$eval('ul.chapter-list', (container) => {
+      const aTag = container
+        ?.querySelector('li')
+        ?.querySelector('div')
+        ?.querySelector('div:last-child')
+        ?.querySelector('a');
+      return aTag?.getAttribute('href');
+    });
 
-      await browser.close();
-      return mangaUrl;
-    } catch (error) {
-      return error;
-    }
+    await browser.close();
+    return mangaUrl;
   }
 }
